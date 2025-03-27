98d41625771ab5980c0f8b3fee8c72981c4e915e
Petkanych-Daryna <kavindarina@gmail.com>
2025-03-27 18:55:10 +0200

    HW-13-js

--- main.js
+++ main.js
@@ -1,4 +1,92 @@
 // Використати деструктуризацію об’єктів у всіх завданнях з попередньої ДЗ
+const user = {
+  mood: "happy",
+  hobby: "play games",
+  premium: true,
+};
+
+const { mood, hobby, premium } = user;
+
+console.log(mood);  
+console.log(hobby); 
+console.log(premium);
+
+const updatedUser = {
+  ...user,            
+  hobby: "skydiving", 
+  premium: false,     
+};
+
+console.log(updatedUser);
+// ---------------
+
+function countProps(obj) {
+  return Object.keys(obj).length; 
+}
+console.log(countProps(user));
+
+// ------------------
+
+function findBestEmployee(employees) {
+  let maxTasks = 0;
+  let bestEmployee = "";
+
+  for (let [name, tasks] of Object.entries(employees)) {
+    if (tasks > maxTasks) {
+      maxTasks = tasks;
+      bestEmployee = name;
+    }
+  }
+
+  return bestEmployee;
+}
+
+let employees = {
+  Liza: 12,
+  Misha: 17,
+  Kate: 6,
+};
+
+console.log(findBestEmployee(employees));
+
+// --------------------------
+
+
+function countTotalSalary(employees) {
+  return Object.values(employees).reduce((total, salary) => total + salary, 0);
+}
+
+let salaries = {
+  Liza: 4000,
+  Misha: 9000,
+  Kate: 1900,
+};
+
+console.log(countTotalSalary(salaries));
+
+// ----------------
+
+function getAllPropValues(arr, prop) {
+  return arr
+  .map((obj) => obj[prop])
+  .filter((value) => value !== undefined); 
+}
+
+let products = [
+  { name: "Apple", price: 40, quantity: 7 },
+  { name: "Banana", price: 62, quantity: 10 },
+  { name: "Orange", price: 88, quantity: 20 },
+];
+
+console.log(getAllPropValues(products, "name"));
+
+// --------------------------
+
+function calculateTotalPrice(allProducts, productName) {
+  let product = allProducts.find((p) => p.name === productName);
+  return product ? product.price * product.quantity : 0;
+}
+console.log(calculateTotalPrice(products, "Apple"));
 
 
 // 2. Напиши сценарій керування особистим кабінетом інтернет-банку. Є об'єкт account в якому необхідно реалізувати методи для роботи з балансом та історією транзакцій.
